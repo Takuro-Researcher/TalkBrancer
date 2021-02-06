cc9ddd5068b5b519072c76ed54a8fc4c23c446f8
Takuro-Researcher <takuro.developer@gmail.com>
2021-02-06 21:32:24 +0900

    Binding＋ViewModel導入

--- app/src/main/java/com/example/talkbrancer/setting/PeopleSettingFragment.kt
+++ app/src/main/java/com/example/talkbrancer/setting/PeopleSettingFragment.kt
@@ -1,26 +1,39 @@
-package com.example.talkbrancer
+package com.example.talkbrancer.setting
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.example.talkbrancer.R
+import com.example.talkbrancer.databinding.FragmentPeopleSettingBinding
 import kotlinx.android.synthetic.main.fragment_people_setting.*
 
 
 class PeopleSettingFragment : Fragment() {
+    private val viewModel: SettingViewModel by activityViewModels()
+    private lateinit var binding: FragmentPeopleSettingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentPeopleSettingBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_people_setting, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.data = viewModel
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -28,5 +41,8 @@ class PeopleSettingFragment : Fragment() {
         back_people_setting_to_title_button.setOnClickListener {
             findNavController().navigate(R.id.action_PeopleSettingFragment_to_TitleFragment)
         }
+        viewModel.peopleCount.observe(viewLifecycleOwner, Observer {
+            System.out.println(it)
+        })
     }
 }
