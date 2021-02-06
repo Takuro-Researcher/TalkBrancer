b87a68051f7c1710055f13b52fa5d35304371ea5
Takuro-Researcher <takuro.developer@gmail.com>
2021-02-06 18:49:24 +0900

    画面遷移＋外枠の画面だけ表示

--- app/src/main/java/com/example/talkbrancer/PeopleSettingFragment.kt
+++ app/src/main/java/com/example/talkbrancer/PeopleSettingFragment.kt
@@ -1,32 +1,32 @@
 package com.example.talkbrancer
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_how_to.*
-import kotlinx.android.synthetic.main.fragment_title.*
+import kotlinx.android.synthetic.main.fragment_people_setting.*
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class HowToFragment : Fragment() {
+
+class PeopleSettingFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_how_to, container, false)
+        return inflater.inflate(R.layout.fragment_people_setting, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        back_howto_to_title_button.setOnClickListener {
-            findNavController().navigate(R.id.action_HowToFragment_to_TitleFragment)
+        back_people_setting_to_title_button.setOnClickListener {
+            findNavController().navigate(R.id.action_PeopleSettingFragment_to_TitleFragment)
         }
     }
 }
