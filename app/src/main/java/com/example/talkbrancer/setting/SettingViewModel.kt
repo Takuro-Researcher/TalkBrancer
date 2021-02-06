328fbdc5777cdb0677a12640722415bda1a217ba
Takuro-Researcher <takuro.developer@gmail.com>
2021-02-06 21:32:59 +0900

    refs #5 追加し忘れたゲーム

---
+++ app/src/main/java/com/example/talkbrancer/setting/SettingViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.talkbrancer.setting
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+
+class SettingViewModel(application: Application): AndroidViewModel(application) {
+    var peopleCount: MutableLiveData<Int> = MutableLiveData()
+
+    init {
+        peopleCount.value = 2
+    }
+}
