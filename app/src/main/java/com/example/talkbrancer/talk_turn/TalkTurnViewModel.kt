fd427163ea16565f1ee6436acbdd3cc4b3cd72c7
Takuro-Researcher <takuro.developer@gmail.com>
2021-02-14 16:07:59 +0900

    アニメーションの開始座標を変更

--- app/src/main/java/com/example/talkbrancer/talk_turn/TalkTurnViewModel.kt
+++ app/src/main/java/com/example/talkbrancer/talk_turn/TalkTurnViewModel.kt
@@ -3,6 +3,8 @@ package com.example.talkbrancer.talk_turn
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.talkbrancer.setting.TalkTheme
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 // who_flag:0 = 話したい。　who_flag:1 = 聞きたい
 data class TalkSessionData(val name: String, val talkTheme: Pair<Int, String>, val who_flag: Int) {}
@@ -10,6 +12,7 @@ data class TalkSessionData(val name: String, val talkTheme: Pair<Int, String>, v
 class TalkTurnViewModel(talkThemeSettingData: ArrayList<TalkTheme>) : ViewModel() {
     val talkThemeList: MutableList<TalkSessionData> = mutableListOf()
     val isEndAction: MutableLiveData<Unit> = MutableLiveData()
+    private val alreadyTalkTheme: MutableList<TalkSessionData> = mutableListOf()
 
     init {
         emptyRemovingTalkTheme(talkThemeSettingData)
@@ -27,5 +30,9 @@ class TalkTurnViewModel(talkThemeSettingData: ArrayList<TalkTheme>) : ViewModel(
         if (talkThemeList.size == 0) {
             isEndAction.value = Unit
         }
+    }
+
+    private fun decideTalkSession() {
+        val randomIndex = Random.nextInt(0..10)
     }
 }
